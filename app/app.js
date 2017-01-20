21f064225e801af4b6b4529be761c92409c4282c
unicitydev <srober45@uwyo.edu>
2017-01-20 09:13:37 -0700

    Routing skeleton done

--- app/app.js
+++ app/app.js
@@ -1,49 +1,56 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('cadpractice', [
-  'ui-router'
-]).
-config(function($stateProvider) {
+var myApp = angular.module('cadprac', ['ui.router']);
+myApp.config(function($stateProvider) {
     var spotState = {
         name: 'Spots',
-        url: 'spots',
+        url: '/spots',
         template: '<h3>This is where the spots live!</h3>'
-    }
+    };
 
     var orderState = {
         name: 'Orders',
-        url: 'orders',
+        url: '/orders',
         template: '<h3>This is where the orders live!</h3>'
-    }
+    };
 
     var destinationState = {
         name: 'Destinations',
-        url: 'destinations',
+        url: '/destinations',
         template: '<h3>This is where the destinations live!</h3>'
-    }
+    };
 
     var settingsState = {
         name: 'Settings',
-        url: 'settings',
+        url: '/settings',
         template: '<h3>This is where the settings live!</h3>'
-    }
+    };
 
     var supportState = {
         name: 'Support',
-        url: 'support',
+        url: '/support',
         template: '<h3>This is where the support lives!</h3>'
-    }
+    };
 
     var adminState = {
         name: 'Admin',
-        url: 'admin',
+        url: '/admin',
         template: '<h3>This is where the admin panel lives!</h3>'
-    }
+    };
 
     var homeState = {
         name: 'Home',
-        url: 'home',
+        url: '/home',
         template: '<h3>This is where the CTS home page lives!</h3>'
-    }
+    };
+
+    $stateProvider.state(spotState);
+    $stateProvider.state(orderState);
+    $stateProvider.state(destinationState);
+    $stateProvider.state(settingsState);
+    $stateProvider.state(supportState);
+    $stateProvider.state(adminState);
+    $stateProvider.state(homeState);
+
 });
