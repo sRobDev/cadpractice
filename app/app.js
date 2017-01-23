dc1b79dc2e28fccc94656f47f02f0480ea7355e0
unicitydev <srober45@uwyo.edu>
2017-01-22 19:27:46 -0700

    All functionality done

--- app/app.js
+++ app/app.js
@@ -1,48 +1,56 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-var myApp = angular.module('cadprac', ['ui.router']);
+var myApp = angular.module('cadprac', ['ui.router', 'ui.grid']);
 myApp.config(function($stateProvider) {
     var spotState = {
         name: 'Spots',
         url: '/spots',
-        template: '<h3>This is where the spots live!</h3>'
+        templateUrl: 'views/spotsview/spots-template.html',
+        controller: 'spotsController',
+        data: {pageTitle: 'Spots'}
     };
 
     var orderState = {
         name: 'Orders',
         url: '/orders',
-        template: '<h3>This is where the orders live!</h3>'
+        templateUrl: 'views/ordersview/orders-template.html',
+        data: {pageTitle: 'Orders'}
     };
 
     var destinationState = {
         name: 'Destinations',
         url: '/destinations',
-        template: '<h3>This is where the destinations live!</h3>'
+        templateUrl: 'views/destinationsview/destionations-template.html',
+        data: {pageTitle: 'Destinations'}
     };
 
     var settingsState = {
         name: 'Settings',
         url: '/settings',
-        template: '<h3>This is where the settings live!</h3>'
+        template: '<h3>This is where the settings live!</h3>',
+        data: {pageTitle: 'Settings'}
     };
 
     var supportState = {
         name: 'Support',
         url: '/support',
-        template: '<h3>This is where the support lives!</h3>'
+        template: '<h3>This is where the support lives!</h3>',
+        data: {pageTitle: 'Support'}
     };
 
     var adminState = {
         name: 'Admin',
         url: '/admin',
-        template: '<h3>This is where the admin panel lives!</h3>'
+        template: '<h3>This is where the admin panel lives!</h3>',
+        data: {pageTitle: 'Admin'}
     };
 
     var homeState = {
         name: 'CTSHome',
         url: '/ctshome',
-        template: '<h3>This is where the CTS home page lives!</h3>'
+        template: '<h3>This is where the CTS home page lives!</h3>',
+        data: {pageTitle: 'CTS Home'}
     };
 
     $stateProvider.state(spotState);
@@ -54,3 +62,99 @@ myApp.config(function($stateProvider) {
     $stateProvider.state(homeState);
 
 });
+
+myApp.run([ '$rootScope', '$state', '$stateParams',
+    function ($rootScope, $state, $stateParams) {
+        $rootScope.$state = $state;
+        $rootScope.$stateParams = $stateParams;
+    }]);
+
+myApp.controller('spotsController', ['$scope', function($scope){
+    $scope.spotsData = [
+        {
+            "Spot #": '6176',
+            "Length": "30s",
+            "Sender": "NBCUniversal"
+        },
+        {
+            "Spot #": '2368',
+            "Length": "60s",
+            "Sender": "SONY"
+        },
+        {
+            "Spot #": '6510',
+            "Length": "45s",
+            "Sender": "LG"
+        },
+        {
+            "Spot #": '9439',
+            "Length": "30s",
+            "Sender": "XFINITY"
+        },
+        {
+            "Spot #": '1337',
+            "Length": "120s",
+            "Sender": "SCHMUCKS"
+        }
+    ]
+}]);
+
+myApp.controller('ordersController', ['$scope', function($scope){
+    $scope.ordersData = [
+        {
+            "Order #": '6176',
+            "# of Spots": "3",
+            "Receiver": "NBCUniversal"
+        },
+        {
+            "Order #": '2368',
+            "# of Spots": "1",
+            "Receiver": "SONY"
+        },
+        {
+            "Order #": '6510',
+            "# of Spots": "3",
+            "Receiver": "LG"
+        },
+        {
+            "Order #": '9439',
+            "# of Spots": "3",
+            "Receiver": "XFINITY"
+        },
+        {
+            "Order #": '6626',
+            "# of Spots": "7",
+            "Receiver": "SCHMUCKS"
+        }
+    ]
+}]);
+
+myApp.controller('destinationsController', ['$scope', function($scope){
+    $scope.destinationsData = [
+        {
+            "iDont": 'So it\'s',
+            "KnowWhat": "3",
+            "DataToPut": "NBCUniversal"
+        },
+        {
+            "iDont": 'The',
+            "KnowWhat": "1",
+            "DataToPut": "SONY"
+        },
+        {
+            "iDont": 'Same',
+            "KnowWhat": "3",
+            "DataToPut": "LG"
+        },
+        {
+            "iDont": 'As',
+            "KnowWhat": "3",
+            "DataToPut": "XFINITY"
+        },
+        {
+            "iDont": 'Orders',
+            "KnowWhat": "7",
+            "DataToPut": "SCHMUCKS"
+        }
+    ]
+}]);
